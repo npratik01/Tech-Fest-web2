0c08d14a4d0192618cff02d840de61fb82579469
npratik01 <2241038@gcoej.ac.in>
2025-11-07 08:41:01 +0530

    Update footer copyright notice to include '@' symbol before Technical Council

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -183,7 +183,7 @@ const Footer = () => {
         {/* Bottom Bar */}
         <div className="border-t border-gray-800 pt-8 flex flex-col items-center gap-3">
           <p className="text-gray-400 text-sm text-center">
-            All rights are reserved by Technical Council
+            All rights are reserved by @Technical Council
           </p>
           <p className="text-gray-400 text-sm text-center">
             Developed by{" "}
